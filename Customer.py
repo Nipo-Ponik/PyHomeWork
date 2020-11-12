480e01a14c5be1b5b1626300111a4d038bcb1616
Ponik <73924559+Nipo-Ponik@users.noreply.github.com>
2020-11-12 16:14:59 +0700

    Lombard simulation

---
+++ Customer.py
@@ -0,0 +1,7 @@
+class Customer():
+
+    def __init__(self, item, day_in, name):
+        self._item = item
+        self._day_in = day_in
+        self._name = name
+
---
+++ Item.py
@@ -0,0 +1,8 @@
+class Item():
+
+    def __init__(self, name, cost, day):
+        self._name = name
+        self._cost = cost
+        self._day = day
+
+
