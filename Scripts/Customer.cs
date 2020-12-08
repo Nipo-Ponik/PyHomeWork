1da6a6cb006524b957199e27a1dace4eb1090018
Ponik <73924559+Nipo-Ponik@users.noreply.github.com>
2020-12-08 14:54:47 +0700

    Add files via upload

---
+++ Scripts/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Enums;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class Customer : MonoBehaviour
+{
+    private bool _isRegular;
+    private bool _isWaiting;
+    private readonly LevelEn _level;
+
+    public Customer(LevelEn level, bool isRegular)
+    {
+        _level = level;
+        _isRegular = isRegular;
+    }
+
+    public LevelEn Level { get; set; }
+    public bool IsWaiting { get; set; }
+    public bool IsRegular { get; set; }
+
+    public Haircut ChoiceHaircut()    // Generate & choice
+    {
+        Array values = Enum.GetValues(typeof(HaircutEn));
+        int rand = Random.Range(1, values.Length);
+        HaircutEn randomHaircut = (HaircutEn)values.GetValue(rand);
+
+        int cost;
+        int time;
+        if (rand % (values.Length / 3) == 0)
+        {
+            cost = 100 * (rand - 1) ^ 2; // 100 - start cost 
+            time = 1 * (rand - 1) ^ 2;    // 1 hour - start time: junior - 1 hour, middle - 2...
+        }
+        else
+        {
+            cost = 100 * rand ^ 2;
+            time = 1 * rand ^ 2;  
+        }
+        
+        Haircut haircut = new Haircut(_level, cost, time);
+        return haircut;
+    }
+    
+    public void ToStartPosition(Vector3 position)
+    {
+        gameObject.transform.position = position;
+    }
+}
---
+++ Scripts/MoveCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] 
+    private Vector3 secondPosition;
+    
+    public void Move()
+    {
+        gameObject.transform.position = gameObject.transform.position != secondPosition ? secondPosition : 
+            new Vector3(0, secondPosition.y, secondPosition.z);
+    }
+}
